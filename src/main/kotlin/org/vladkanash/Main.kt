c1b09642a16b6fb5e49cff3b9c70a665ec2c97a8
Vlad Kanash <vlad.kanash@gmail.com>
2021-07-17 10:28:55 +0200

    Initial commit. Add basic implementation

---
+++ src/main/kotlin/org/vladkanash/Main.kt
@@ -0,0 +1,56 @@
+package org.vladkanash
+
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.time.LocalDate
+import java.time.LocalDate.now
+import java.time.temporal.ChronoUnit
+import java.time.temporal.ChronoUnit.DAYS
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+
+var date: LocalDate = LocalDate.of(2020, 12, 13)
+
+@ExperimentalTime
+fun main() {
+    val botToken = System.getenv("TOKEN")
+    val kovzikId = System.getenv("KOVZIK_ID").toLong()
+    val chatId = System.getenv("GUMBALL_CHAT_ID")
+
+    val bot = bot {
+        token = botToken
+        dispatch {
+            text {
+                message
+                    .takeIf { it.from?.id == kovzikId }
+                    ?.also { date = now() }
+            }
+        }
+    }
+
+    bot.startPolling()
+
+    runBlocking {
+        while (isActive) {
+            launch {
+                bot.sendMessage(
+                    ChatId.fromId(chatId.toLong()),
+                    text = getMessage(getDaysPassed(date))
+                )
+            }
+            delay(Duration.days(1))
+        }
+    }
+}
+
+private fun getDaysPassed(date: LocalDate) = DAYS.between(date, now())
+
+private fun getMessage(days: Long) = """
+    Прошло уже $days дней с тех пор как Николай общался со своими хорошими друзьями в этой конфе :(
+""".trimIndent()
