71f18bf6f6bd93fdbb05658516a8851004001dc7
Vlad Kanash <vlad.kanash@gmail.com>
2021-08-26 21:44:25 +0200

    Add updater function

---
+++ src/main/kotlin/org/vladkanash/function/Updater.kt
@@ -0,0 +1,12 @@
+package org.vladkanash.function
+
+import com.google.cloud.functions.HttpFunction
+import com.google.cloud.functions.HttpRequest
+import com.google.cloud.functions.HttpResponse
+
+class Updater : HttpFunction {
+
+    override fun service(request: HttpRequest?, response: HttpResponse?) {
+        TODO("Not yet implemented")
+    }
+}
