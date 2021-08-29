9836f97081821a942aea043f99b110843ed94301
Vlad Kanash <vlad.kanash@gmail.com>
2021-08-29 12:56:20 +0200

    Finished updater function

--- src/main/kotlin/org/vladkanash/function/Updater.kt
+++ src/main/kotlin/org/vladkanash/function/Updater.kt
@@ -1,12 +1,52 @@
 package org.vladkanash.function
 
+import com.github.kotlintelegrambot.entities.Update
+import com.github.kotlintelegrambot.network.serialization.GsonFactory
 import com.google.cloud.functions.HttpFunction
 import com.google.cloud.functions.HttpRequest
 import com.google.cloud.functions.HttpResponse
+import kotlinx.datetime.Clock.System.now
+import kotlinx.datetime.TimeZone.Companion.currentSystemDefault
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.ExperimentalSerializationApi
+import org.vladkanash.repository.Firebase
+import org.vladkanash.repository.Message
 
-class Updater : HttpFunction {
+@Suppress("unused")
+@ExperimentalSerializationApi
+class Updater(
+    private val firebase: Firebase = Firebase()
+) : HttpFunction {
 
-    override fun service(request: HttpRequest?, response: HttpResponse?) {
-        TODO("Not yet implemented")
+    private val gson = GsonFactory.createForApiClient()
+
+    override fun service(request: HttpRequest, response: HttpResponse) {
+        verifyToken(request)
+
+        val update = request.getUpdate()
+        println("Update is $update")
+        handleUpdate(update)
+        request.reader.close()
     }
+
+    private fun HttpRequest.getUpdate() =
+        reader.readText().let { gson.fromJson(it, Update::class.java) }
+
+    private fun handleUpdate(update: Update) {
+        val kovzikId = System.getenv("KOVZIK_ID").toLong()
+        if (update.message?.from?.id == kovzikId) {
+            firebase.updateLastMessage(
+                Message(update.message?.text ?: "", today())
+            )
+            println("Last message updated")
+        }
+    }
+
+    private fun verifyToken(request: HttpRequest) {
+        val requestToken = request.queryParameters["token"].orEmpty().firstOrNull()
+        val savedToken = System.getenv("VERIFY_TOKEN")
+        if (requestToken != savedToken) throw IllegalArgumentException()
+    }
+
+    private fun today() = now().toLocalDateTime(currentSystemDefault()).date
 }
